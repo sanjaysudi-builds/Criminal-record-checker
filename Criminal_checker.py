9a23c5b010bfc079db79e20b095678cfeb7c1716
SANJU S <sanjaysudi32@gmail.com>
2025-10-19 23:21:35 +0530

    Implement criminal name and number checker

--- Criminal_checker.py
+++ Criminal_checker.py
@@ -1,1 +1,8 @@
-
+correct_criminalname = "dany"
+correct_criminalnumber= "420"
+criminalname = input("Enter criminal name: ").lower()
+criminalnumber = input("Enter  criminal number: ")
+if criminalname == correct_criminalname and criminalnumber == correct_criminalnumber:
+    print("Criminal found in the police station list.")
+else:
+    print("Criminal not found in the police station list.")
